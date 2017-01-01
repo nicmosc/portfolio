07f0878ac683c6bd7f02d12871a832502a35a284
nicmosc <nmoscholios@gmail.com>
2017-01-01 22:57:10 +0100

    add twitter meta

--- src/global-context.js
+++ src/global-context.js
@@ -1,4 +1,19 @@
 module.exports = {
-  meta_title: 'Nicolaos Moscholios | Creative Software Engineer',
-  meta_description: '',
+
+  meta: {
+    title: 'Nicolaos Moscholios | Creative Software Engineer',
+    description: "Hello. My name is Nick. I'm a recent Computer Science graduate currently working at Drawbotics, with a passion for UI and 3D design.",
+    facebook: {
+      img_url: '',
+      site_name: 'Nicolaos Moscholios'
+    },
+    twitter: {
+      img_url: '/static/images/favicons/apple-touch-icon-120x120.png',
+      at: '@nicmosc',
+      'img_alt'
+    },
+    linkedin: {
+      img_url: '/static/images/favicons/apple-touch-icon-120x120.png',
+    },
+  }
 }
