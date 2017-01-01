32aede96949e08836247b9b9a49a58779831561d
nicmosc <nmoscholios@gmail.com>
2017-01-01 23:06:52 +0100

    add facebook meta & small avatar imgs

--- src/global-context.js
+++ src/global-context.js
@@ -4,16 +4,14 @@ module.exports = {
     title: 'Nicolaos Moscholios | Creative Software Engineer',
     description: "Hello. My name is Nick. I'm a recent Computer Science graduate currently working at Drawbotics, with a passion for UI and 3D design.",
     facebook: {
-      img_url: '',
-      site_name: 'Nicolaos Moscholios'
+      img_url: '/static/images/personal-avatar-fb.png',
+      site_name: 'Nicolaos Moscholios',
+      profile_id: 'nicola.moscholios',
     },
     twitter: {
-      img_url: '/static/images/favicons/apple-touch-icon-120x120.png',
+      img_url: '/static/images/personal-avatar-twitter.png',
       at: '@nicmosc',
-      'img_alt'
-    },
-    linkedin: {
-      img_url: '/static/images/favicons/apple-touch-icon-120x120.png',
+      img_alt: 'Moscholios Logo',
     },
   }
 }
