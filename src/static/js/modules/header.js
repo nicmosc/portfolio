a54c021d592fba08c18fdbeb6b3fa00acaf139a4
nicmosc <nmoscholios@gmail.com>
2017-01-08 20:50:02 +0100

    add scroll to section functionality in header

--- src/static/js/modules/header.js
+++ src/static/js/modules/header.js
@@ -5,23 +5,38 @@ module.exports = (function($) {
   var header;
   var buttons;
   var logo;
+  var menuContainer;
 
   function init() {
     header = $('.Header');
-    logo = $('.Header__logoContainer');
-    buttons = $('.Header__menuContainer');
+    logo = header.find('.Header__logoContainer');
+    menuContainer = header.find('.Header__menuContainer');
+    buttons = header.find('.Header__button');
+
+    console.log(buttons);
 
     _attachEvents();
   }
 
   function _attachEvents() {
-    logo.click(_handleLogoClicked);
+    logo.click(_handleClickLogo);
+    buttons.click(_handleClickButton);
   }
 
-  function _handleLogoClicked() {
-    buttons.toggleClass('Header__menuContainer--hidden');
+  function _handleClickLogo() {
+    menuContainer.toggleClass('Header__menuContainer--hidden');
     header.toggleClass('Header--hidden');
     logo.toggleClass('Header__logoContainer--active');
+  }
+
+  function _handleClickButton() {
+    console.log(header.height());
+    var targetId = $(this).data('scroll-to');
+    var target = $('#'+targetId);
+    $('html,body').animate({
+      scrollTop: (target.offset().top - header.outerHeight())
+    }, 1000);
+    return false;
   }
 
   return {
