ad54c8392d93518825625fde86f4241483538017
nicmosc <nmoscholios@gmail.com>
2017-01-08 13:07:32 +0100

    dont use const

--- docs/bundle/app.bundle.js
+++ docs/bundle/app.bundle.js
@@ -92,32 +92,40 @@
 	module.exports = function ($) {
 	
 	  var loadingContainer;
+	  var backgroundContainer;
+	  var header;
 	  var lastBrick;
 	
 	  function init() {
 	    loadingContainer = $('.LegoContainer');
 	    lastBrick = $('.top-front');
+	    backgroundContainer = $('.Container');
+	    header = $('.Header');
 	
-	    console.log(loadingContainer, lastBrick);
 	    _attachEvents();
 	  }
 	
 	  function _attachEvents() {
+	    _hideOnBeginLoading();
 	    _hideOnEndLoading();
 	  }
 	
 	  function _hideOnEndLoading() {
-	
 	    lastBrick.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function (e) {
 	      console.log('animation finished');
 	      loadingContainer.fadeOut('slow');
 	    });
 	  }
 	
+	  function _hideOnBeginLoading() {
+	    // header.hide();
+	    // backgroundContainer.hide();
+	  }
+	
 	  return {
 	    init: init
 	  };
-	}(_jquery2.default); // var jQuery = require('jquery');
+	}(_jquery2.default);
 
 /***/ },
 
--- src/static/js/modules/loading-legos.js
+++ src/static/js/modules/loading-legos.js
@@ -1,30 +1,36 @@
-// var jQuery = require('jquery');
 import jQuery from 'jquery';
 
 module.exports = (function($) {
 
   var loadingContainer;
+  var backgroundContainer;
+  var header;
   var lastBrick;
 
   function init() {
     loadingContainer = $('.LegoContainer');
     lastBrick = $('.top-front');
+    backgroundContainer = $('.Container');
+    header = $('.Header');
 
-
-    console.log(loadingContainer, lastBrick);
     _attachEvents();
   }
 
   function _attachEvents() {
+    _hideOnBeginLoading();
     _hideOnEndLoading();
   }
 
   function _hideOnEndLoading() {
-
     lastBrick.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
       console.log('animation finished');
       loadingContainer.fadeOut('slow');
     });
+  }
+
+  function _hideOnBeginLoading() {
+    // header.hide();
+    // backgroundContainer.hide();
   }
 
   return {
